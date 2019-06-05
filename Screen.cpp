be3f08d62781336b9dcdf4cdbfb23c13f8525594
Barman <33091564+smushis@users.noreply.github.com>
2019-06-05 11:31:12 +0200

    Update Screen.cpp

--- Screen.cpp
+++ Screen.cpp
@@ -1,4 +1,8 @@
 #include "Screen.h"
+#include <mraa/i2c.h>
+#include <unistd.h>
+#include <iostream>
+#include <stdlib.h>
 
 #define LCD_CLEARDISPLAY 0x01
 #define LCD_RETURNHOME 0x02
@@ -21,6 +25,8 @@ int m_bus=0;
 int m_lcd_control_addr = 0x3E;
 int m_rgb_control_addr = 0x62;
 mraa_i2c_context m_i2c_lcd_control, m_i2c_lcd_rgb;
+
+// @Fonctions transfert data en I2C
 
 mraa_result_t i2cData (mraa_i2c_context ctx, uint8_t value) {
     mraa_result_t error = MRAA_SUCCESS;
@@ -52,11 +58,11 @@ mraa_result_t i2cReg (mraa_i2c_context ctx, int deviceAdress, int addr, uint8_t 
     return error;
 }
 
-
-Screen::Screen(){
-	this->initScreen();	
+// Constructeur de notre screen
+Screen::Screen(){		
 }
 
+//@Fonction d'initialisation de l'écran et de la comm d'I2C
 void Screen::initScreen(){
 
     // initialisation
@@ -86,31 +92,37 @@ void Screen::initScreen(){
     i2cReg (m_i2c_lcd_rgb, m_rgb_control_addr, 0x03, 100);
     i2cReg (m_i2c_lcd_rgb, m_rgb_control_addr, 0x02, 100);
 
-} 
-void Screen::write(string s){
+}
+
+// Fonction pour écrire sur l'écran  avec un string donné
+void Screen::Ecrire(string s){
 	for(string::size_type i = 0; i < s.size(); i++){
 		i2cData (m_i2c_lcd_control, s[i]);
 	}
 }
 
-void Screen::displayScore(Player player){
-	this->home();
+// Fonction d'affichage du score d'un player
+void Screen::afficherScore(Player player){
     char buffer[10];
     sprintf(buffer,"Score : %d",player.getScore());
     string msg = string(buffer);
-	this->write(msg);
+	this->Ecrire(msg);
 }
 
+// Fonction pour effacer l'écran
 void Screen::clearScreen(){
     i2Cmd (m_i2c_lcd_control, LCD_CLEARDISPLAY);
     usleep(4500);
 }
 
+// Fonction pour retourner le curseur au début de l'écran
 void Screen::home(){
     i2Cmd (m_i2c_lcd_control, LCD_RETURNHOME);
     usleep(4500);	
 }
-void Screen::setCursor(int row, int column){
+
+// Fonction pour choisir la position du curseur sur l'écran
+void Screen::nbLine(int row, int column){
     int row_addr[] = { 0x80, 0xc0, 0x14, 0x54};
     uint8_t offset = ((column % 16) + row_addr[row]);
     i2Cmd (m_i2c_lcd_control, offset);	
